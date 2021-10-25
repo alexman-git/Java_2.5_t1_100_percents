9f431a934fafd6cb84de15afadffb31c173ab7a8
Alexander Maletin <man.alex212@gmail.com>
2021-10-25 23:38:28 +0500

    Code coverage ratio with lines is 1.00

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ class StatisticsServiceTest {
     void shouldFindMaxIncome() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {5, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
